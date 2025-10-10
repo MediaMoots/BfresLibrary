74b8272c8dd86fd7d34c4ca2f00c1941acbf14f9
MediaMoots <81146974+MediaMoots@users.noreply.github.com>
2025-10-11 00:13:26 +0800

    Update KeyShape.cs

--- BfresLibrary/Shared/Models/Shape/KeyShape.cs
+++ BfresLibrary/Shared/Models/Shape/KeyShape.cs
@@ -8,20 +8,16 @@ namespace BfresLibrary
 
         public byte[] TargetAttribIndices { get; set; }
 
-        public byte[] TargetAttribIndexOffsets { get; set; }
-
         // ---- METHODS ------------------------------------------------------------------------------------------------
 
         void IResData.Load(ResFileLoader loader)
         {
             TargetAttribIndices = loader.ReadBytes(20);
-            TargetAttribIndexOffsets = loader.ReadBytes(4);
         }
         
         void IResData.Save(ResFileSaver saver)
         {
             saver.Write(TargetAttribIndices);
-            saver.Write(TargetAttribIndexOffsets);
         }
     }
 }
